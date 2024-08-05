9d318e084739ca258c674be484b3b2589f23097f
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2024-08-05 14:56:56 +0400

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketPairs = Object.fromEntries(bracketsConfig);
+  const openBrackets = new Set(bracketsConfig.map(pair => pair[0]));
+  const closeBrackets = new Set(bracketsConfig.map(pair => pair[1]));
+  
+  for (const char of str) {
+    if (openBrackets.has(char)) {
+      if (closeBrackets.has(char) && stack.length && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (closeBrackets.has(char)) {
+      if (!stack.length || bracketPairs[stack.pop()] !== char) {
+        return false;
+      }
+    }
+  }
+  
+  return stack.length === 0;
 }
